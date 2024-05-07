2961d0d80d11ca6ca78b4c7d236a0cf353569aa8
peter-lyr <llydrp.ldp@gmail.com>
2024-05-08 00:23:58 +0800

    commit info: escape

--- lua/dp_test.lua
+++ lua/dp_test.lua
@@ -58,6 +58,12 @@ function M.dp_plugins()
 
   function M.add_commit_push_dot()
     local info = vim.fn.input('commit info: ', '.')
+    info = string.gsub(info, '>', '^>')
+    info = string.gsub(info, '<', '^<')
+    info = string.gsub(info, '%^', '^^')
+    info = string.gsub(info, '%%', '^%')
+    info = string.gsub(info, '&', '^&')
+    info = string.gsub(info, '|', '^|')
     if not B.is(info) then
       print 'Canceled, commit info is Empty'
       return
